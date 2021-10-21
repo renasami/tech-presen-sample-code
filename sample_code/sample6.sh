72653c0460b052510abee8d0beac65df3ae2bbca
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-10-21 14:03:35 +0900

    auto commit

--- sample_code/sample6.sh
+++ sample_code/sample6.sh
@@ -1,11 +1,19 @@
-# 条件分岐if
+# 繰り返しfor
 
-for i in $(seq ${1:-"10"}); do
-    if [ $((${i}%3)) == 0 ]; then  # shellでは四則演算、論理演算がややこしいです！詳しくはhandoutのURLより
-        echo "Buzz"
-    else
-        echo ${i}
-    fi
+# 文法はこれだよ
+# for i in item1 item2 item3 ....; do
+#     command1
+#     command3
+#     command2
+#     ...
+# done
+
+for i in a b c d 4; do
+    echo "${i}"
 done
 
+echo ""
 
+for i in $(seq ${1:-"10"}); do
+    echo "${i}"
+done
