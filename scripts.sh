3612ada147f052de39e76130766c87b0e9c500be
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-10-21 15:07:35 +0900

    delete token

--- scripts.sh
+++ scripts.sh
@@ -3,10 +3,10 @@ docker build -t github-auto-grass-grower .
 docker run -itd \
 -e TZ="Asia/Tokyo" \
 -e GITHUB_USERNAME=KuroiCc \
--e GITHUB_TOKEN=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 \
--e repo_url=https://github.com/KuroiCc/tech-presen-sample-code.git \
--e email=chenchengmikuro@gmail.com \
+-e GITHUB_TOKEN=token \
+-e repo_url=url \
+-e email=youremall \
 -e gpu_schedule="* * * * *" \
--e commit_mesg="auto commit chen cheng" \
+-e commit_mesg="auto commit" \
 --name github-auto-grass-grower \
 github-auto-grass-grower
