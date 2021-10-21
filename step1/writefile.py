edb3d945e4b794d26ebfa0edcc49c5b9ed97377d
renasami <drrr0502@gmail.com>
2021-10-21 18:09:40 +0900

    auto commit

--- step1/writefile.py
+++ step1/writefile.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+import random
 import sys
 
 if __name__ == "__main__":
     path = sys.argv[1]
     text = datetime.now().strftime("%Y-%m-%d %X") + "\n"
-
+    text += random.random()
     with open(path, "a") as file:
         file.write(text)
