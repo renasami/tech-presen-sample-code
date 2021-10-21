224055e029525aa98b0f86123e25274759c0fd7d
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-10-21 14:05:55 +0900

    auto commit

--- step1/auto_commit.sh
+++ step1/auto_commit.sh
@@ -1,6 +1,6 @@
 # cd /repo
 git pull
-python3 ./step1/scripts/writefile.py "${records_file_path:-"./records.txt"}"
+python3 ./step1/writefile.py "${records_file_path:-"./records.txt"}"
 git add .
 git commit -m "${commit_mesg:-"auto commit"}"
 git push
